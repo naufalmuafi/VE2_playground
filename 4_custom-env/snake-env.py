9272d9182a8f75a216ece618d82d47459a0155d5
Naufal Muafi <nmuafi1@gmail.com>
2024-05-05 15:21:26 +0700

    add: reset() method

---
+++ 4_custom-env/snake-env.py
@@ -0,0 +1,76 @@
+import gymnasium as gym
+import numpy as np
+from gymnasium import spaces
+import numpy as np
+import cv2
+import random
+import time
+from collections import deque
+
+SNAKE_LEN_GOAL = 30
+
+def collision_with_apple(apple_position, score):
+    apple_position = [random.randrange(1,50)*10,random.randrange(1,50)*10]
+    score += 1
+    return apple_position, score
+
+def collision_with_boundaries(snake_head):
+    if snake_head[0]>=500 or snake_head[0]<0 or snake_head[1]>=500 or snake_head[1]<0 :
+        return 1
+    else:
+        return 0
+
+def collision_with_self(snake_position):
+    snake_head = snake_position[0]
+    if snake_head in snake_position[1:]:
+        return 1
+    else:
+        return 0
+
+class SnakeGameEnv(gym.Env):
+    """Custom Environment that follows gym interface."""
+
+    metadata = {"render_modes": ["human"], "render_fps": 30}
+
+    def __init__(self):
+        super(SnakeGameEnv, self).__init__()
+        # Define action and observation spacejj
+        # They must be gym.spaces objects
+        # Example when using discrete actions:
+        self.action_space = spaces.Discrete(4)
+        # Example for using image as input (channel-first; channel-last also works):
+        self.observation_space = spaces.Box(low=0, high=255,
+                                            shape=(N_CHANNELS, HEIGHT, WIDTH), dtype=np.uint8)
+
+    def step(self, action):
+        ...
+        return observation, reward, terminated, truncated, info
+
+    def reset(self, seed=None, options=None):
+        self.done = False
+        self.img = np.zeros((500,500,3),dtype='uint8')
+
+        # Initial Snake and Apple position
+        self.snake_position = [[250,250], [240,250], [230,250]]
+        self.apple_position = [random.randrange(1,50)*10, random.randrange(1,50)*10]
+        self.score = 0
+        self.prev_button_direction = 1
+        self.button_direction = 1
+        self.snake_head = [250,250]
+        
+        # head_x, head_y, apple_delta_, apple_delta_y, snake_length, prev_direction
+        head_x = self.snake_head[0]
+        head_y = self.snake_head[1]
+        
+        apple_delta_x = head_x - self.apple_position[0]
+        apple_delta_y = head_y - self.apple_position[1]
+        
+        snake_length = len(self.snake_position)
+        
+        self.prev_actions = deque(maxlen=SNAKE_LEN_GOAL)
+        for _ in range(SNAKE_LEN_GOAL):
+            self.prev_actions(-1)
+        
+        self.observation = [head_x, head_y, apple_delta_x, apple_delta_y, snake_length] + list(self.prev_actions)
+        
+        return self.observation, self.info
