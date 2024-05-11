f9ac25db05f7c2fb14a0e830f16319145a41ffbf
Naufal Muafi <nmuafi1@gmail.com>
2024-05-11 19:47:11 +0700

    add: train_A2C() function and add os library

--- 4_custom-env/warehouse_robot/warehouse_robot_train.py
+++ 4_custom-env/warehouse_robot/warehouse_robot_train.py
@@ -2,7 +2,31 @@
 Using Stable-Baselines3 to train The Warehouse Robot
 '''
 
+import os
 import numpy as np
 import gymnasium as gym
 import warehouse_robot_env
 from stable_baselines3 import A2C, PPO
+
+# train with A2C Algoritm
+def train_A2C():
+    # where to store trained model and logs
+    model_dir = 'models'
+    log_dir = 'logs'
+    os.makedirs(model_dir, exist_ok=True)
+    os.makedirs(log_dir, exist_ok=True)
+    
+    env = gym.make('WarehouseRobot-v0')
+    
+    # use Advantage Actor-Critic (A2C) algorithm
+    # use MLP policy for observation space 1D-vector
+    model = A2C('MlpPolicy', env, verbose=1, tensorboard_log=log_dir)
+    
+    TIMESTEPS = 1000
+    iters = 0
+    
+    while True:
+      iters += 1
+      
+      model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False)
+      model.save(f'{model_dir}/a2c_{TIMESTEPS * iters}')
