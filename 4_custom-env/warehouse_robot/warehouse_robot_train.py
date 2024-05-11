c1f81ac387cba7a1de2567ad8a88ff17fc726df9
Naufal Muafi <nmuafi1@gmail.com>
2024-05-11 23:16:53 +0700

    DONE: tested with A2C algorithm in a2c_18000

--- 4_custom-env/warehouse_robot/warehouse_robot_train.py
+++ 4_custom-env/warehouse_robot/warehouse_robot_train.py
@@ -10,46 +10,46 @@ from stable_baselines3 import A2C, PPO
 
 # train with A2C Algoritm
 def train_A2C():
-    # where to store trained model and logs
-    model_dir = 'models'
-    log_dir = 'logs'
-    os.makedirs(model_dir, exist_ok=True)
-    os.makedirs(log_dir, exist_ok=True)
+  # where to store trained model and logs
+  model_dir = 'models'
+  log_dir = 'logs'
+  os.makedirs(model_dir, exist_ok=True)
+  os.makedirs(log_dir, exist_ok=True)
     
-    env = gym.make('WarehouseRobot-v0')
+  env = gym.make('WarehouseRobot-v0')
     
-    # use Advantage Actor-Critic (A2C) algorithm
-    # use MLP policy for observation space 1D-vector
-    model = A2C('MlpPolicy', env, verbose=1, tensorboard_log=log_dir)
+  # use Advantage Actor-Critic (A2C) algorithm
+  # use MLP policy for observation space 1D-vector
+  model = A2C('MlpPolicy', env, verbose=1, tensorboard_log=log_dir)
     
-    TIMESTEPS = 1000
-    iters = 0
+  TIMESTEPS = 1000
+  iters = 0
     
-    while True:
-      iters += 1
+  while True:
+    iters += 1
       
-      model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False)
-      model.save(f'{model_dir}/a2c_{TIMESTEPS * iters}')
+    model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False)
+    model.save(f'{model_dir}/a2c_{TIMESTEPS * iters}')
 
 # test with A2C algorithm
 def test_A2C(render=True):
-    env = gym.make('WarehouseRobot-v0', render_mode='human' if render else None)
+  env = gym.make('WarehouseRobot-v0', render_mode='human' if render else None)
     
-    # load the model
-    model = A2C.load('models/a2c_2000', env=env)
+  # load the model
+  model = A2C.load('models/a2c_15000', env=env)
     
-    # run a test
-    obs = env.reset()[0]
-    done = False
-    while not done:
-        action, _states = model.predict(obs, deterministic=True) # deterministic=True means no random action
-        obs, _rewards, done, _, _info = env.step(action)
+  # run a test
+  obs = env.reset()[0]
+  done = False
+  while not done:
+    action, _states = model.predict(obs, deterministic=True) # deterministic=True means no random action
+    obs, _rewards, done, _, _info = env.step(action)
         
-        if done:
-          break
+    if done:
+      break
 
 # unit testing
 if __name__ == '__main__':
     # train and test the model with A2C algorithm
-    train_A2C()
-    # test_A2C()
+    # train_A2C()
+    test_A2C()
