de961d8c96e9d20d0554f5ab9e9f0e732b2e7c04
Naufal Muafi <nmuafi1@gmail.com>
2024-05-11 18:04:17 +0700

    add: WarehouseRobotEnv with it's constructor

--- 4_custom-env/warehouse_robot/warehouse_robot_env.py
+++ 4_custom-env/warehouse_robot/warehouse_robot_env.py
@@ -17,3 +17,32 @@ register(
   entry_point='warehouse_robot:WarehouseRobotEnv',
 )
 
+# Implement our own gym env, must inherit from gym.Env
+# https://gymnasium.farama.org/api/env/
+
+class WarehouseRobotEnv(gym.Env):
+  # metadata is a required attribute
+  # render_modes in our environment is either None or 'human'.
+  # render_fps is not used in our env, but we are require to declare a non-zero value.
+  metadata = {"render_modes": ["human"], 'render_fps': 4}
+  
+  def __init__(self, grid_rows=4, grid_cols=5, render_mode=None):
+    self.grid_rows = grid_rows
+    self.grid_cols = grid_cols
+    self.render_mode = render_mode
+    
+    # initialize the warehouse problem
+    self.warehouse_robot = wr.WarehouseRobot(grid_rows, grid_cols, self.metadata['render_fps'])
+    
+    # define the action space i.e. the possible actions the agent/the robot's can take
+    self.action_space = spaces.Discrete(len(wr.RobotAction))
+    
+    # define the observation space i.e. the possible states/posiitons the agent/the robot can be in
+    # used to validate the observation returned by reset() and step()
+    # use 1D-vector to represent the robot's position: : [robot_row_pos, robot_col_pos, target_row_pos, target_col_pos]
+    self.observation_space = spaces.Box(
+      low=0, 
+      high=np.array([self.grid_rows-1, self.grid_cols-1, self.grid_rows-1, self.grid_cols-1]), 
+      shape=(4, ), 
+      dtype=np.int32
+    )
