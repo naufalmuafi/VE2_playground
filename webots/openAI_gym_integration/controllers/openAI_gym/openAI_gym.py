9e0586ec9ea60778bdd1e9b91300b81c957d4846
Naufal Muafi <nmuafi1@gmail.com>
2024-05-25 15:25:43 +0700

    add: unit testing for openAI_gym.py

--- webots/openAI_gym_integration/controllers/openAI_gym/openAI_gym.py
+++ webots/openAI_gym_integration/controllers/openAI_gym/openAI_gym.py
@@ -6,3 +6,29 @@ Reinforcement Learning
 with Stable-Baselines3
 '''
 
+import sys
+
+try:
+  import gymnasium as gym
+  from stable_baselines3 import PPO
+except ImportError:
+  sys.exit(
+		'Please make sure you have all dependencies installed!'
+	)
+
+# instance the environment
+env = gym.make()
+
+# load the model
+model = PPO.load('models', env=env)
+
+# run the model in the agent environment
+obs = env.reset()[0]
+
+for _ in range(100000):
+  action, _states = model.predict(obs)
+  obs, reward, done, _, _info = env.step(action)
+  print(obs, reward, done)
+  
+  if done:
+    obs = env.reset()[0]
