d92f9de25e146b285c2cf954476630f40d9a0e9e
Naufal Muafi <nmuafi1@gmail.com>
2024-05-25 07:40:12 +0700

    add: supervisor controller programming structure

--- webots/supervisor/controllers/supervisor_controller/supervisor_controller.py
+++ webots/supervisor/controllers/supervisor_controller/supervisor_controller.py
@@ -1,32 +1,13 @@
-"""supervisor_controller controller."""
+from controller import Supervisor # type: ignore
 
-# You may need to import some classes of the controller module. Ex:
-#  from controller import Robot, Motor, DistanceSensor
-from controller import Robot
+TIME_STEP = 32
 
-# create the Robot instance.
-robot = Robot()
+robot = Supervisor()  # create Supervisor instance
 
-# get the time step of the current world.
-timestep = int(robot.getBasicTimeStep())
+# [CODE PLACEHOLDER 1]
 
-# You should insert a getDevice-like function in order to get the
-# instance of a device of the robot. Something like:
-#  motor = robot.getDevice('motorname')
-#  ds = robot.getDevice('dsname')
-#  ds.enable(timestep)
+i = 0
+while robot.step(TIME_STEP) != -1:
+  # [CODE PLACEHOLDER 2]
 
-# Main loop:
-# - perform simulation steps until Webots is stopping the controller
-while robot.step(timestep) != -1:
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
-
-    # Process sensor data here.
-
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
-    pass
-
-# Enter here exit cleanup code.
+  i += 1
