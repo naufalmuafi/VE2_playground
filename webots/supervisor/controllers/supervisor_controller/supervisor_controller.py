5c62c03f2823b99e6f737aa79722aa61c5787fe8
Naufal Muafi <nmuafi1@gmail.com>
2024-05-25 08:47:05 +0700

    add: moving objects using a supervisor

--- webots/supervisor/controllers/supervisor_controller/supervisor_controller.py
+++ webots/supervisor/controllers/supervisor_controller/supervisor_controller.py
@@ -4,10 +4,13 @@ TIME_STEP = 32
 
 robot = Supervisor()  # create Supervisor instance
 
-# [CODE PLACEHOLDER 1]
+bb8_node = robot.getFromDef('BB-8')  # get the BB8 node
+translation_field = bb8_node.getField('translation')  # get the translation field of the BB8 node
 
 i = 0
 while robot.step(TIME_STEP) != -1:
-  # [CODE PLACEHOLDER 2]
+  if i == 0:
+      new_value = [2.5, 0, 0]
+      translation_field.setSFVec3f(new_value)
 
   i += 1
