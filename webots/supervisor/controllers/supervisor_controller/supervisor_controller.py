b120712bc7c40c07f4d54b20d3c62d710112d057
Naufal Muafi <nmuafi1@gmail.com>
2024-05-25 09:03:25 +0700

    add: spawning and removing the node w/ supervisor

--- webots/supervisor/controllers/supervisor_controller/supervisor_controller.py
+++ webots/supervisor/controllers/supervisor_controller/supervisor_controller.py
@@ -7,10 +7,22 @@ robot = Supervisor()  # create Supervisor instance
 bb8_node = robot.getFromDef('BB-8')  # get the BB8 node
 translation_field = bb8_node.getField('translation')  # get the translation field of the BB8 node
 
+root_node = robot.getRoot()  # get the root node
+children_field = root_node.getField('children')  # get the children field of the root node
+
 i = 0
 while robot.step(TIME_STEP) != -1:
+  # move the BB-8 robot
   if i == 0:
-      new_value = [2.5, 0, 0]
-      translation_field.setSFVec3f(new_value)
+    new_value = [2.5, 0, 0]
+    translation_field.setSFVec3f(new_value)
+  
+  # removing the robot node
+  if i == 10:
+    bb8_node.remove()
+  
+  # spawning the robot
+  if i == 20:
+    children_field.importMFNodeFromString(-1, 'Nao { translation 2.5 0 0.334 }')
 
   i += 1
