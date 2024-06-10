a92708ff0109cf979e6baf1b235b9a65fc989af3
Naufal Muafi <nmuafi1@gmail.com>
2024-06-11 01:08:23 +0700

    add: requirements of dependencies

--- webots/openAI_gym_integration/controllers/pioneer3at_controller_env/pioneer3at_controller_env.py
+++ webots/openAI_gym_integration/controllers/pioneer3at_controller_env/pioneer3at_controller_env.py
@@ -1,24 +1,153 @@
-"""pioneer3at_controller_env controller."""
+import sys
+from controller import Supervisor
+from gymnasium.envs.registration import register
 
-# You may need to import some classes of the controller module. Ex:
-#  from controller import Robot, Motor, DistanceSensor
-from controller import Robot
+try:
+    import gymnasium as gym
+    import numpy as np
+    from stable_baselines3 import PPO
+    from stable_baselines3.common.env_checker import check_env
+except ImportError:
+    sys.exit(
+        "Please make sure you have all dependencies installed. "
+        'Run: "pip install numpy gymnasium stable_baselines3"'
+    )
 
-# create the Robot instance.
-robot = Robot()
 
-# get the time step of the current world.
-timestep = int(robot.getBasicTimeStep())
+class OpenAIGymEnvironment(Supervisor, gym.Env):
+    def __init__(self, max_episode_steps=1000):
+        super().__init__()
 
-# You should insert a getDevice-like function in order to get the
-# instance of a device of the robot. Something like:
-#  motor = robot.getDevice('motorname')
-#  ds = robot.getDevice('dsname')
-#  ds.enable(timestep)
+        # Gymnasium generic
+        self.theta_threshold_radians = 0.2
+        self.x_threshold = 0.3
+        high = np.array(
+            [
+                self.x_threshold * 2,
+                np.finfo(np.float32).max,
+                self.theta_threshold_radians * 2,
+                np.finfo(np.float32).max,
+            ],
+            dtype=np.float32,
+        )
+        self.action_space = gym.spaces.Discrete(2)
+        self.observation_space = gym.spaces.Box(-high, high, dtype=np.float32)
+        self.state = None
+        self.spec = gym.envs.registration.EnvSpec(
+            id="WebotsEnv-v0", max_episode_steps=max_episode_steps
+        )
 
-# Main loop:
-# - perform simulation steps until Webots is stopping the controller
-while robot.step(timestep) != -1:
-    print(f"Setup Environment Succussful!")
+        # Environment specific
+        self.__timestep = int(self.getBasicTimeStep())
+        self.__wheels = []
+        self.__pendulum_sensor = None
 
-# Enter here exit cleanup code.
+        # Tools
+        self.keyboard = self.getKeyboard()
+        self.keyboard.enable(self.__timestep)
+
+    def wait_keyboard(self):
+        while self.keyboard.getKey() != ord("Y"):
+            super().step(self.__timestep)
+
+    def reset(self, seed=None, options=None):
+        # Reset the simulation
+        self.simulationResetPhysics()
+        self.simulationReset()
+        super().step(self.__timestep)
+
+        # Motors
+        self.__wheels = []
+        for name in [
+            "back left wheel",
+            "back right wheel",
+            "front left wheel",
+            "front right wheel",
+        ]:
+            wheel = self.getDevice(name)
+            wheel.setPosition(float("inf"))
+            wheel.setVelocity(0)
+            self.__wheels.append(wheel)
+
+        # Sensors
+        self.__pendulum_sensor = self.getDevice("position sensor")
+        self.__pendulum_sensor.enable(self.__timestep)
+
+        # Internals
+        super().step(self.__timestep)
+
+        # Gymnasium generic
+        self.state = np.array([0, 0, 0, 0]).astype(np.float32)
+        return self.state, {}
+
+    def step(self, action):
+        # Execute the action
+        for wheel in self.__wheels:
+            wheel.setVelocity(1.3 if action == 1 else -1.3)
+        super().step(self.__timestep)
+
+        # Observation
+        robot = self.getSelf()
+        endpoint = self.getFromDef("POLE_ENDPOINT")
+        self.state = np.array(
+            [
+                robot.getPosition()[0],
+                robot.getVelocity()[0],
+                self.__pendulum_sensor.getValue(),
+                endpoint.getVelocity()[4],
+            ]
+        )
+
+        # Done
+        done = bool(
+            self.state[0] < -self.x_threshold
+            or self.state[0] > self.x_threshold
+            or self.state[2] < -self.theta_threshold_radians
+            or self.state[2] > self.theta_threshold_radians
+        )
+
+        # Reward
+        reward = 0 if done else 1
+
+        return self.state.astype(np.float32), reward, done, {}, {}
+
+    def render(self, mode="human"):
+        pass
+
+
+# Register the environment
+def make_env():
+    return OpenAIGymEnvironment()
+
+
+register(
+    id="WebotsEnv-v0",
+    entry_point=make_env,
+    max_episode_steps=1000,
+)
+
+
+def main():
+    # Initialize the environment
+    env = gym.make("WebotsEnv-v0")
+    check_env(env)
+
+    # Train
+    model = PPO("MlpPolicy", env, n_steps=2048, verbose=1)
+    model.learn(total_timesteps=1e5)
+
+    # Replay
+    print("Training is finished, press `Y` for replay...")
+    env.wait_keyboard()
+
+    obs, _ = env.reset()
+    for _ in range(100000):
+        action, _states = model.predict(obs)
+        obs, reward, done, _, _ = env.step(action)
+        print(obs, reward, done)
+        if done:
+            obs, _ = env.reset()
+
+
+if __name__ == "__main__":
+    main()
