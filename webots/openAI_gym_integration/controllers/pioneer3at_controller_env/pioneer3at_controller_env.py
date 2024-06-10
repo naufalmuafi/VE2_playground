a5e6c83a312561054c7db6463dcef64ef65364b4
Naufal Muafi <nmuafi1@gmail.com>
2024-06-11 00:14:23 +0700

    add: virtual environment and set up the folder with webots

--- webots/openAI_gym_integration/controllers/pioneer3at_controller_env/pioneer3at_controller_env.py
+++ webots/openAI_gym_integration/controllers/pioneer3at_controller_env/pioneer3at_controller_env.py
@@ -19,14 +19,6 @@ timestep = int(robot.getBasicTimeStep())
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
 while robot.step(timestep) != -1:
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
-
-    # Process sensor data here.
-
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
-    pass
+    print(f"Setup Environment Succussful!")
 
 # Enter here exit cleanup code.
