6ba5810f14fae88d757149250672c7ef84f94356
Naufal Muafi <nmuafi1@gmail.com>
2024-07-09 18:59:03 +0700

    add: worlds defined with image recognition

--- webots/camera_segmentation/controllers/camera_segmentation_py/camera_segmentation_py.py
+++ webots/camera_segmentation/controllers/camera_segmentation_py/camera_segmentation_py.py
@@ -1,9 +1,19 @@
+# Copyright 1996-2023 Cyberbotics Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """
-
-finder_controller for finding the object project.
-
-Using Webots and Stable Baselines3.
-
+An example of use of a camera device with recognition capability.
 """
 
 from controller import Robot, Display
@@ -11,36 +21,23 @@ from controller import Robot, Display
 
 class Controller(Robot):
     def __init__(self):
-        # Initialize the Robot class
         super(Controller, self).__init__()
-
-        # Set the time step
         self.timeStep = 64
-
-        # set the speed of the motors
-        self.speed = 1.5
-
-        # Get the camera device
         self.camera = self.getDevice("camera")
         self.camera.enable(self.timeStep)
         self.camera.recognitionEnable(self.timeStep)
         self.camera.enableRecognitionSegmentation()
-
-        # Get the display device
         self.display = self.getDevice("segmented image display")
-
-        # Get the left and right wheel motors
         self.left_motor = self.getDevice("left wheel motor")
         self.right_motor = self.getDevice("right wheel motor")
         self.left_motor.setPosition(float("inf"))
         self.right_motor.setPosition(float("inf"))
-        self.left_motor.setVelocity(-self.speed)
-        self.right_motor.setVelocity(self.speed)
+        self.left_motor.setVelocity(-1.5)
+        self.right_motor.setVelocity(1.5)
 
     def run(self):
         width = self.camera.getWidth()
         height = self.camera.getHeight()
-
         while self.step(self.timeStep) != -1:
             if (
                 self.camera.isRecognitionSegmentationEnabled()
@@ -49,7 +46,6 @@ class Controller(Robot):
                 # Get the segmented image and display it in the Display
                 data = self.camera.getRecognitionSegmentationImage()
                 if data:
-                    # print(f"segmented image: {data}")
                     segmented_image = self.display.imageNew(
                         data, Display.BGRA, width, height
                     )
@@ -57,6 +53,5 @@ class Controller(Robot):
                     self.display.imageDelete(segmented_image)
 
 
-if __name__ == "__main__":
-    controller = Controller()
-    controller.run()
+controller = Controller()
+controller.run()
