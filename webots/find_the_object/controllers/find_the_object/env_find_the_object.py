bb33d6566cfced45bc3db575b70ad6cb8cff240f
Naufal Muafi <nmuafi1@gmail.com>
2024-07-09 22:17:59 +0700

    add: create a env gym specification

--- webots/find_the_object/controllers/find_the_object/env_find_the_object.py
+++ webots/find_the_object/controllers/find_the_object/env_find_the_object.py
@@ -6,24 +6,50 @@ Using Webots and Stable Baselines3.
 
 """
 
-from controller import Robot, Display
+import sys
+from controller import Robot, Display, Supervisor
+from typing import Any, Tuple, List
 
 
-class Controller(Robot):
+try:
+    import numpy as np
+    import gymnasium as gym
+    from gymnasium import Env, spaces
+    from gymnasium.envs.registration import EnvSpec, register
+except ImportError:
+    sys.exit(
+        "Please make sure you have all dependencies installed. "
+        "Run: `pip install numpy gymnasium stable_baselines3`"
+    )
+
+
+class FTO_Env(Robot, Supervisor, Env):
     def __init__(self):
         # Initialize the Robot class
-        super(Controller, self).__init__()
+        super().__init__()
+
+        # register the Environment
+        self.spec: EnvSpec = EnvSpec(id="FTO-v1")
 
         # Set the time step
-        self.timeStep = 64
+        self.__timestep: int = int(self.getBasicTimeStep())
+
+        # gym environment specification
+        high: np.ndarray = np.array(
+            [
+                self.camera.getWidth(),
+                self.camera.getHeight(),
+                3,
+            ],
+        )
 
         # set the speed of the motors
         self.speed = 1.5
 
         # Get the camera device
         self.camera = self.getDevice("camera")
-        self.camera.enable(self.timeStep)
-        self.camera.recognitionEnable(self.timeStep)
+        self.camera.enable(self.__timestep)
+        self.camera.recognitionEnable(self.__timestep)
         self.camera.enableRecognitionSegmentation()
 
         # Get the display device
@@ -41,7 +67,7 @@ class Controller(Robot):
         width = self.camera.getWidth()
         height = self.camera.getHeight()
 
-        while self.step(self.timeStep) != -1:
+        while self.step(self.__timestep) != -1:
             if (
                 self.camera.isRecognitionSegmentationEnabled()
                 and self.camera.getRecognitionSamplingPeriod() > 0
@@ -57,6 +83,25 @@ class Controller(Robot):
                     self.display.imageDelete(segmented_image)
 
 
+# helper function to wait for user input
+def wait_for_y():
+    while True:
+        user_input = input("Press 'Y' to continue: ")
+        if user_input.upper() == "Y":
+            print("Continuing process...\n\n")
+            break
+        else:
+            print("Invalid input. Please press 'Y'.")
+
+
+# register the environment
+register(
+    id="FTO-v1",
+    entry_point=lambda: FTO_Env(),
+    max_episode_steps=1000,
+)
+
+
 if __name__ == "__main__":
-    controller = Controller()
-    controller.run()
+    env = FTO_Env()
+    env.run()
